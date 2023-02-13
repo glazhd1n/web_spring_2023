5e037a5e737197a09bfd9838dceee89e6910889e
glazhd1n <mrgreenlimon@gmail.com>
2023-02-13 17:55:36 +0600

    Added createTask function

--- lab 3/a0.js
+++ lab 3/a0.js
@@ -2,9 +2,38 @@ const toDoList = [];
 
 const addTask = () => {
     const form = document.getElementById("NewTask");
-    console.log(form.value)
-    toDoList.push(form.value)
+    if(form.value.length != 0) {
+        createTask(form);
+    }
 };
+
+const createTask = (form) => {
+    const hed = document.getElementById("tasks");
+    const newElement = document.createElement("div");
+    const deleteButton = createDeleteButton();
+    newElement.appendChild(deleteButton);
+    deleteButton.classList.add("deleteButton");
+    newElement.innerHTML = form.value;
+    newElement.classList.add("task1");
+    hed.appendChild(newElement);
+    newElement.appendChild(deleteButton)
+}
+
+const createCheckBox = () => {
+    const check = document.createElement("");
+}
+
+
+const createDeleteButton = () => {
+    const deleteButton = document.createElement("button");
+    const deleteImage = document.createElement("img");
+    deleteImage.src = "assets/trash.png";
+    deleteButton.appendChild(deleteImage);
+    deleteButton.style.appearance = "none";
+    deleteButton.classList.add("trash-button");
+    deleteImage.classList.add("trash");
+    return deleteButton;
+}
 
 const completeTask = () => {
     const task = 13;
@@ -23,19 +52,3 @@ const showList = () => {
         console.log(`${index + 1}. ${task}`);
     });
 };
-
-const changeHower = () => {
-    const form = document.getElementById("NewTask");
-    form.value = "";
-    form.style.color = "black";
-
-}
-addTask("Go grocery shopping");
-addTask("Pay bills");
-addTask("Exercise");
-
-showList();
-
-completeTask("Pay bills");
-
-showList();

9067bc0c62d79f71319149c50d0a9ed86428d27c
glazhd1n <mrgreenlimon@gmail.com>
2023-02-14 22:20:02 +0600

    Finally got to work on the Local Stage, the final version of the project, maybe there will be some edits to the design

--- lab 3/a0.js
+++ lab 3/a0.js
@@ -1,35 +1,50 @@
-const toDoList = [];
-
+let toDoList = [];
+window.addEventListener('load', function () {
+    const arrayToRender = localStorage.getItem('todos');
+    toDoList = JSON.parse(arrayToRender);
+    for(const i of toDoList) {
+        createTask(i);
+    }
+});
 const addTask = () => {
     const form = document.getElementById("NewTask");
-    if(form.value.length != 0) {
-        createTask(form);
+    if(form.value.length !== 0) {
+        createTask(form.value);
+        toDoList.push(form.value);
+        localStorage.setItem("todos", JSON.stringify(toDoList));
+        form.value = "";
     }
 };
 
 const createTask = (form) => {
-    const hed = document.getElementById("tasks");
-    const newElement = document.createElement("div");
+    const hed          = document.getElementById("tasks");
+    const newElement   = document.createElement("div");
     const deleteButton = createDeleteButton();
-    const checkBox = createCheckBox();
-    console.log(checkBox.value);
+    const checkBox     = createCheckBox();
+    const text         = document.createElement("p");
+
+    text.innerHTML     = form;
     deleteButton.classList.add("deleteButton");
-    newElement.innerHTML = form.value;
     newElement.classList.add("task1");
     hed.appendChild(newElement);
     newElement.appendChild(deleteButton);
+    newElement.appendChild(text);
     newElement.appendChild(checkBox);
 };
 
-const changeStyle = (checkBox) => {
-
+const removeElement = (toRemove) => {
+    console.log(toRemove);
+    const i = toDoList.indexOf(toRemove);
+    console.log(i);
+    toDoList.splice(i);
+    localStorage.setItem("todos", JSON.stringify(toDoList));
 };
 
 const createCheckBox = () => {
     const checkBox = document.createElement("input");
     checkBox.type = "checkbox";
     checkBox.addEventListener("click", function () {
-        if (checkBox.checked == true) {
+        if (checkBox.checked === true) {
             checkBox.parentElement.style.textDecoration = "line-through"
             checkBox.parentElement.style.color = "gray";
         } else {
@@ -51,11 +66,13 @@ const createDeleteButton = () => {
     deleteImage.classList.add("trash");
 
     deleteButton.addEventListener("click", function () {
+        const parEl = deleteButton.parentElement.children[1];
+        removeElement(parEl.innerText);
         deleteButton.parentElement.remove();
     })
 
     return deleteButton;
-}
+};
 
 const completeTask = () => {
     const task = 13;

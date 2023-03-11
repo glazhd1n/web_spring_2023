1f6099ada0a3872427f152570817b1d5c3a75fec
glazhd1n <mrgreenlimon@gmail.com>
2023-03-11 11:30:58 +0600

    added about albums and home components and added to router

--- Lab6/src/app/app.module.ts
+++ Lab6/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {TopBarComponent} from "./top-bar/top-bar.component";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AlbumsComponent } from './albums/albums.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    HomeComponent,
+    AboutComponent,
+    AlbumsComponent
   ],
   imports: [
     BrowserModule,

f4710b4255ef173f652165245f5bada2cf06af31
glazhd1n <mrgreenlimon@gmail.com>
2023-04-05 17:13:57 +0600

    Added name display instead of Product Object (N) or Category Object (N)

--- Lab 8/shopback/api/models.py
+++ Lab 8/shopback/api/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
@@ -10,3 +13,6 @@ class Product(models.Model):
     count = models.IntegerField()
     is_active = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name

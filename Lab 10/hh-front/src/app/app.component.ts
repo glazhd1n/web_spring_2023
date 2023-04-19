89c380647eb50cb41790532a48d58be602ded2ac
glazhd1n <mrgreenlimon@gmail.com>
2023-04-19 19:10:06 +0600

    back added

--- Lab 10/hh-front/src/app/app.component.ts
+++ Lab 10/hh-front/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Company } from './company';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hh-front';
+  selectedCompany: Company | undefined;
+
+  onSelect(company: Company): void {
+    this.selectedCompany = company;
+  }
+  
+
 }
--- Lab 10/hh-front/src/app/app.module.ts
+++ Lab 10/hh-front/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompanyListComponent } from './company-list/company-list.component';
+import { VacancyListComponent } from './vacancy-list/vacancy-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyListComponent,
+    VacancyListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
